79d01d4b25b679d79685fa19e85111046d087917
Rajeshwari-Argulwar <98045986+Rajeshwari-Argulwar@users.noreply.github.com>
2022-02-04 10:34:08 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+from math import floor, sqrt
+
+
+phase = "Hello"
+print(phase)
+print(phase.lower())
+print(phase.upper())
+print(phase.isupper())
+print(phase.islower())
+print(len(phase))
+print(phase.index("o"))
+print(phase.replace("hello","hi"))
+
+
+
+
+num = input()
+print(type(num))
+print(pow(5,5))
+print(max(5,2,6,9,10))
+print(min(5,8,4,7,1,3,6))
+print(round(num))
+print(sqrt(85))
+print(floor(52.63))
